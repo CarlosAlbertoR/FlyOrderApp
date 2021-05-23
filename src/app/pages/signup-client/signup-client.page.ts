13e6082c38fbc066e8a7bb722469be7aca6c11c4
CarlosAlbertoR <carlosalbertorios3@gmail.com>
2021-05-22 21:32:56 -0500

    The fields of the signup for were validates

--- src/app/pages/signup-client/signup-client.page.ts
+++ src/app/pages/signup-client/signup-client.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Client } from 'src/app/models/client';
@@ -12,17 +13,31 @@ import { SignupService } from 'src/app/services/signup.service';
 export class SignupClientPage implements OnInit {
 
   client = {} as Client;
+  form: FormGroup;
 
-  constructor(private router: Router, private signupService: SignupService) { }
+  constructor(
+    private router: Router,
+    private signupService: SignupService,
+    private builder: FormBuilder
+  ) { }
 
   register(client: Client) {
     console.log(client);
     let postConsult: Observable<any>;
     postConsult = this.signupService.executePostRequest('/signupClient', client);
-    postConsult.subscribe(res => {});
+    postConsult.subscribe(res => { });
   }
 
   ngOnInit() {
+    this.form = this.builder.group({
+      name: ['', [Validators.required, Validators.minLength(6), Validators.pattern(/^[a-zA-Z ]+$/)]],
+      address: ['', [Validators.required, Validators.minLength(14)]],
+      city: ['', [Validators.required, Validators.pattern(/^[a-zA-Z ]+$/)]],
+      phone: ['', [Validators.required, Validators.pattern(/^(?:[0-9] ?){9,14}[0-9]$/)]],
+      email: ['', [Validators.required, Validators.pattern(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)]],
+      password: ['', [Validators.minLength(8), Validators.required, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')]],
+      password_check:  ['', [Validators.minLength(8), Validators.required, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')]]
+    });
   }
 
   routingSignin() {
