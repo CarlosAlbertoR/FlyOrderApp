d83c521369307dd7e26e53c6966eac85074b31e3
CarlosAlbertoR <carlosalbertorios3@gmail.com>
2021-05-23 16:58:06 -0500

    The fields of the signup for were validates with the submit button

--- src/app/pages/signup-client/signup-client.page.ts
+++ src/app/pages/signup-client/signup-client.page.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Client } from 'src/app/models/client';
 import { SignupService } from 'src/app/services/signup.service';
+import { ConfirmedValidator } from './confirmed.validator';
 
 @Component({
   selector: 'app-signup-client',
@@ -30,14 +31,20 @@ export class SignupClientPage implements OnInit {
 
   ngOnInit() {
     this.form = this.builder.group({
-      name: ['', [Validators.required, Validators.minLength(6), Validators.pattern(/^[a-zA-Z ]+$/)]],
+      name: ['', [Validators.required, Validators.pattern(/^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{6,}$/)]],
       address: ['', [Validators.required, Validators.minLength(14)]],
-      city: ['', [Validators.required, Validators.pattern(/^[a-zA-Z ]+$/)]],
+      city: ['', [Validators.required, Validators.pattern(/^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$/)]],
       phone: ['', [Validators.required, Validators.pattern(/^(?:[0-9] ?){9,14}[0-9]$/)]],
       email: ['', [Validators.required, Validators.pattern(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)]],
       password: ['', [Validators.minLength(8), Validators.required, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')]],
-      password_check:  ['', [Validators.minLength(8), Validators.required, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')]]
-    });
+      password_check: ['', [Validators.required]]
+    },
+      { validator: ConfirmedValidator('password', 'password_check') }
+    );
+  }
+
+  checkPasswords(password, password_check) { 
+    return password === password_check ? null : { notSame: true }
   }
 
   routingSignin() {
@@ -45,3 +52,4 @@ export class SignupClientPage implements OnInit {
   }
 
 }
+
